bc4ae302e404aa3ba3077efab1f08ca04a4af11e
Travis Bock <tbock@innovateteam.com>
2019-11-20 18:49:33 -0800

    adjusting prod to new url

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
     oauth_url: 'https://epa.maps.arcgis.com/sharing/rest/oauth2/authorize',
   oauth_client_id: 'QAHRbKsgqdjDctXU',
   oauth_response_type: 'token',
-  oauth_redirect_uri: 'https://r9.ercloud.org/request/oauthcallback',
+  oauth_redirect_uri: 'https://request.ercloud.org/oauthcallback',
   local_client_id: 'QSeQUnG4NqLFJC5PKAPBdrkb0E0IIqR5uYZxbr8s',
-  local_service_endpoint: 'https://r9.ercloud.org/request/api',
+  local_service_endpoint: 'https://request.ercloud.org/api',
   recaptcha_siteKey: '6LfUaYEUAAAAAKX5g1YTIXqNsmtAnMK16nnJvSit'
 };
