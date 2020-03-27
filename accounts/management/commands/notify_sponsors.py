6bd4325cc9d0840191973937c32e59b71f5e84f8
Jenny Holder <jlholder90@gmail.com>
2020-03-27 09:12:13 -0400

    update sponsor email text

--- accounts/management/commands/notify_sponsors.py
+++ accounts/management/commands/notify_sponsors.py
@@ -14,13 +14,13 @@ class Command(BaseCommand):
         for notification in pending_notifications:
             results = send_mail(
                 'Pending GeoPlatform Account Requests',
-                f'You have {notification["total"]} pending account requests. '
-                f'Please go to https://r9.ercloud.org/request/accounts/list to review',
+                f'{notification["total"]} GeoPlatform Account Request(s) is pending your approval in the GeoPlatform Account Request Tool(request.ercloud.org).'
+                f'Please log in to the <a href="https://request.ercloud.org/accounts/list">GeoPlatform Account Request Tool Approval List</a> to configure new GeoPlatform accounts and notify the users when configuration is complete.',
                 'GIS_Team@epa.gov',
                 [notification['sponsor__email']],
                 fail_silently=False,
-                html_message=f'You have {notification["total"]} pending account requests. Please go to '
-                f'<a href="https://r9.ercloud.org/request/accounts/list">https://r9.ercloud.org/request/accounts/list</a> to review',
+                html_message=f'{notification["total"]} GeoPlatform Account Request(s) is pending your approval in the GeoPlatform Account Request Tool(request.ercloud.org).'
+                f'Please log in to the <a href="https://request.ercloud.org/accounts/list">GeoPlatform Account Request Tool Approval List</a> to configure new GeoPlatform accounts and notify the users when configuration is complete.',
             )
             if results == 1:
                 AccountRequests.objects.filter(
