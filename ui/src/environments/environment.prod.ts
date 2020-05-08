57df733a60ff5ab5c8aa5677076fab790443ffb9
Travis Bock <tbock@innovateteam.com>
2020-05-08 14:15:32 -0700

    fix prod env

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  oauth_url: 'https://epa.maps.arcgis.com/sharing/rest/oauth2/authorize',
+  oauth_url: 'https://requests.ercloud.org/api/oauth2',
   local_service_endpoint: 'https://request.ercloud.org/api',
   api_url: 'https://request.ercloud.org/api',
   recaptcha_siteKey: '6LfUaYEUAAAAAKX5g1YTIXqNsmtAnMK16nnJvSit'
