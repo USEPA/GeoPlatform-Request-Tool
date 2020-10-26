8b1af7dac0e1516aecec8fd19c756f2077e0294b
Travis Bock <tbock@innovateteam.com>
2020-10-26 12:37:40 -0700

    add google analytics

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {LoginService} from './auth/login.service';
 import {BehaviorSubject, Observable, ReplaySubject, Subject} from 'rxjs';
-import {Router} from '@angular/router';
+import {NavigationEnd, Router} from '@angular/router';
 import {UserConfig, UserConfigService} from './auth/user-config.service';
 import {environment} from '../environments/environment';
+import {filter} from 'rxjs/operators';
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -13,7 +16,19 @@ import {environment} from '../environments/environment';
 export class AppComponent implements OnInit {
   admin_url: string;
   config: UserConfig;
-  constructor(public loginService: LoginService, public router: Router, public userConfig: UserConfigService) {}
+
+  constructor(public loginService: LoginService, public router: Router, public userConfig: UserConfigService) {
+    const navEndEvent$ = router.events.pipe(
+      filter(e => e instanceof NavigationEnd)
+    );
+    navEndEvent$.subscribe((e: NavigationEnd) => {
+      window['dataLayer'].push({
+        event: 'navigation',
+        pageName: e.url
+      });
+    });
+  }
+
   ngOnInit(): void {
     this.admin_url = `${environment.api_url}/admin/`;
     this.userConfig.config.subscribe(config => this.config = config);
