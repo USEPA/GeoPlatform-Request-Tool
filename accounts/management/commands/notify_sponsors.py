22db764290c598696ee89a775df5611867a0708a
Jenny Holder <jlholder90@gmail.com>
2021-07-14 10:27:11 -0400

    update approval notice

--- accounts/management/commands/notify_sponsors.py
+++ accounts/management/commands/notify_sponsors.py
@@ -16,12 +16,12 @@ class Command(BaseCommand):
             to_emails.append(notification['sponsor__email'])
             results = send_mail(
                 'Pending GeoPlatform Account Requests',
-                f'{notification["total"]} GeoPlatform Account Request(s) is pending your approval in the GeoPlatform Account Request Tool(request.ercloud.org).'
+                f'{notification["total"]} GeoPlatform Account Request(s) is pending your approval in the GeoPlatform Account Request Tool.'
                 f'Please log in to the <a href="https://r9data.response.epa.gov/request/accounts/list">GeoPlatform Account Request Tool Approval List</a> to configure new GeoPlatform accounts and notify the users when configuration is complete.',
                 'GIS_Team@epa.gov',
                 list(set(to_emails)),
                 fail_silently=False,
-                html_message=f'{notification["total"]} GeoPlatform Account Request(s) is pending your approval in the GeoPlatform Account Request Tool(request.ercloud.org).'
+                html_message=f'{notification["total"]} GeoPlatform Account Request(s) is pending your approval in the GeoPlatform Account Request Tool.'
                 f'Please log in to the <a href="https://r9data.response.epa.gov/request/accounts/list">GeoPlatform Account Request Tool Approval List</a> to configure new GeoPlatform accounts and notify the users when configuration is complete.',
             )
             if results == 1:
