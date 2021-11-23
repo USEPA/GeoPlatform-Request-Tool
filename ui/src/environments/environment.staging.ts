c66a82c74cbfae79b99c30f8f4b6c84741b11e8a
Travis Bock <tbock@innovateteam.com>
2021-11-23 13:11:58 -0800

    fix typo

--- ui/src/environments/environment.staging.ts
+++ ui/src/environments/environment.staging.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   oauth_url: 'https://r9data.response.epa.gov/apps/staging/request/api/oauth2',
   local_service_endpoint: 'https://r9data.response.epa.gov/apps/staging/request/api',
-  api_url: 'https://r9data.response.epa.gov/apps/staging/requests/api',
+  api_url: 'https://r9data.response.epa.gov/apps/staging/request/api',
   recaptcha_siteKey: '6LfUaYEUAAAAAKX5g1YTIXqNsmtAnMK16nnJvSit',
   snackbar_duration: 6000
 };
