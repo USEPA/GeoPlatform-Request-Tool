88e819f8a2be850bb6f8e91eb5f3af393c51df37
Travis Bock <tbock@innovateteam.com>
2023-02-17 14:06:31 -0800

    adjust staging setting endpoitns

--- ui/src/environments/environment.ngst-staging.ts
+++ ui/src/environments/environment.ngst-staging.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  oauth_url: 'https://raisz.epa.gov/apps/staging/request/api/oauth2',
-  local_service_endpoint: 'https://raisz.epa.gov/apps/staging/request/api',
-  api_url: 'https://raisz.epa.gov/apps/staging/request/api',
+  oauth_url: 'https://raisz.epa.gov/request/api/oauth2',
+  local_service_endpoint: 'https://raisz.epa.gov/request/api',
+  api_url: 'https://raisz.epa.gov/request/api',
   recaptcha_siteKey: '6LfUaYEUAAAAAKX5g1YTIXqNsmtAnMK16nnJvSit',
   snackbar_duration: 6000
 };
