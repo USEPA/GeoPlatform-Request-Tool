913c6c6687f1cd883da714499ae1a54bc9263933
Travis Bock <tbock@innovateteam.com>
2023-05-17 09:17:56 -0700

    use fake recaptcha code for staging

--- ui/src/environments/environment.ngst-staging.ts
+++ ui/src/environments/environment.ngst-staging.ts
@@ -3,6 +3,6 @@ export const environment = {
   oauth_url: 'https://raisz.epa.gov/request/api/oauth2',
   local_service_endpoint: 'https://raisz.epa.gov/request/api',
   api_url: 'https://raisz.epa.gov/request/api',
-  recaptcha_siteKey: '6LfUaYEUAAAAAKX5g1YTIXqNsmtAnMK16nnJvSit',
+  recaptcha_siteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
   snackbar_duration: 6000
 };
