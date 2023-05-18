bb15bbf54ad5c623020296ece3355352531a26ff
Travis Bock <tbock@innovateteam.com>
2023-05-18 10:19:36 -0700

    forgot to update staging env based on previous changes

--- ui/src/environments/environment.ngst-staging.ts
+++ ui/src/environments/environment.ngst-staging.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: true,
   oauth_url: 'https://raisz.epa.gov/request/api/oauth2',
-  local_service_endpoint: 'https://raisz.epa.gov/request/api',
-  api_url: 'https://raisz.epa.gov/request/api',
+  local_service_endpoint: '/request/api',
   recaptcha_siteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
   snackbar_duration: 6000
 };
