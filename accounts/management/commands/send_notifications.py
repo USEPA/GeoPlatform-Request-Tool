4ebdfa36c9ef9b9dc225a487c3f1522c92df96e5
Travis Bock <tbock@innovateteam.com>
2023-05-18 15:15:32 -0700

    add ability to test command logging

--- accounts/management/commands/send_notifications.py
+++ accounts/management/commands/send_notifications.py
@@ -5,6 +5,12 @@ from accounts.models import Notification
 class Command(BaseCommand):
     help = 'Send unsent notifications'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--error-test', action='store_true')
+
     def handle(self, *args, **options):
+        if options.get('error_test', False):
+            raise Exception('this should log')
+
         for notification in Notification.objects.filter(sent__isnull=True):
             notification.send()
