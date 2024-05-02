a38864c55b89bf7dedb9944437c760a56e263243
Travis <tbock@innovateteam.com>
2024-05-02 12:57:23 -0700

    new env for ngst-prod

---
+++ ui/src/environments/environment.ngst-prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  oauth_url: 'https://gpdashboard.epa.gov/request/api/oauth2',
+  local_service_endpoint: '/request/api',
+  recaptcha_siteKey: '6Lev_vEoAAAAAAL-XT_NvvmRWQBTHL1Cig_Z6cQU',
+  snackbar_duration: 6000
+};
