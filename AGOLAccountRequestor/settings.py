2183443ff4a1ba026d2d1badcdece4ffb0d54b79
Travis <tbock@innovateteam.com>
2024-09-10 14:12:58 -0700

    add default secret for testing

--- AGOLAccountRequestor/settings.py
+++ AGOLAccountRequestor/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'sssssh-its-a-secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
