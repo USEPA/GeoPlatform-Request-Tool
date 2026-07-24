1caedcb5614665a070fd8feefae98174361eec7f
Travis Bock <tbock@innovateteam.com>
2026-07-23 21:05:24 -0700

    timeout was on wrong step

--- ui/cypress/e2e/approve-error-handling.cy.ts
+++ ui/cypress/e2e/approve-error-handling.cy.ts
@@ -33,8 +33,8 @@ function editRecordToApproveableState() {
   cy.get('mat-cell').first().click();
   cy.get('button#editBtn').click();
   // wait for groups dropdown to become enabled (response must be selected first)
-  cy.get('mat-select[formcontrolname="groups"].mat-select-disabled').should('not.exist');
-  cy.get('mat-select[formcontrolname="groups"]', {timeout: 30000}).click();
+  cy.get('mat-select[formcontrolname="groups"].mat-select-disabled', {timeout: 30000}).should('not.exist');
+  cy.get('mat-select[formcontrolname="groups"]').click();
   cy.get('.mat-option-text').contains('Test').click();
   cy.get('body').click();
   cy.wait(1000); // submit button is briefly disabled
