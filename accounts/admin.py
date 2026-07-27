b49d216b6c0cb17c863b1f44addec8706ccc4038
Travis Bock <tbock@innovateteam.com>
2026-07-27 11:21:08 -0700

    filter default user type choices

--- accounts/admin.py
+++ accounts/admin.py
@@ -34,6 +34,14 @@ def _get_short_name_(user_instance):
 User.get_short_name = _get_short_name_
 
 class AGOLAdminForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Limit default user type options to user types for this AGOL record.
+        if self.instance and self.instance.pk:
+            self.fields['default_user_type'].queryset = UserType.objects.filter(portal=self.instance)
+        else:
+            self.fields['default_user_type'].queryset = UserType.objects.none()
+
     def clean_enterprise_precreate_domains(self):
         valid_emails = []
         for email in self.cleaned_data['enterprise_precreate_domains'].split(','):
